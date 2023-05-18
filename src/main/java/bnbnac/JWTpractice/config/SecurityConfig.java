fb9fff6a3829e6e9973009a48e1039c207570572
bnbnac <bnbnac@naver.com>
2023-05-18 22:50:43 +0900

    feat: set DB, entity

--- src/main/java/bnbnac/JWTpractice/config/SecurityConfig.java
+++ src/main/java/bnbnac/JWTpractice/config/SecurityConfig.java
@@ -1,14 +1,22 @@
 package bnbnac.JWTpractice.config;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring()
+                .requestMatchers(PathRequest.toH2Console());
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -17,6 +25,7 @@ public class SecurityConfig {
                         .requestMatchers("/api/hello").permitAll()
                         .anyRequest().authenticated()
                 );
+
         return http.build();
     }
 }
